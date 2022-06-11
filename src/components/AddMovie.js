51172c14c7958bedf1e6d3109991b1c19c2537b1
RajaSultan <raja.sultan@ceative.co.uk>
2022-06-11 06:40:56 +0500

    filter to be added

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -5,6 +5,7 @@ export const AddMovie = () => {
   const [movies, setMovies] = useContext(movieContext);
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
+
   const inputMovie = (event) => {
     setName(event.target.value);
   };
@@ -22,38 +23,40 @@ export const AddMovie = () => {
       <div className="container">
         <div className="row d-flex justify-content-center">
           <div className="col-6 mt-5 mb-5">
-            <h3 className="text-center">FILL THE MOVIE FORM</h3>
-            <form className="mt-md-5 mt-3" onSubmit={submitData}>
-              <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">
-                  Email Movie Name
-                </label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                  onChange={inputMovie}
-                  value={name}
-                />
-              </div>
-              <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">
-                  Enter Movie Price
-                </label>
-                <input
-                  type="number"
-                  class="form-control"
-                  id="exampleInputPassword1"
-                  onChange={inputPrice}
-                  value={price}
-                />
-              </div>
+            <div className="card pt-4 pb-4 px-5 shadow">
+              <h3 className="text-center">FILL MOVIE FORM</h3>
+              <form className="mt-md-5 mt-3" onSubmit={submitData}>
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">
+                    Email Movie Name
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    onChange={inputMovie}
+                    value={name}
+                  />
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">
+                    Enter Movie Price
+                  </label>
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="exampleInputPassword1"
+                    onChange={inputPrice}
+                    value={price}
+                  />
+                </div>
 
-              <button type="submit" class="btn btn-warning py-2 px-4">
-                Submit
-              </button>
-            </form>
+                <button type="submit" class="btn btn-warning py-2 px-4">
+                  Submit
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Movie = (props) => {
   return (
@@ -6,25 +6,35 @@ const Movie = (props) => {
       <div className="container">
         <div className="row d-flex justify-content-center">
           <div className="col-6 mt-3 mb-5">
-            <h3 className="text-center">MOVIES DATA</h3>
-            <table class="table mt-5 table-bordered">
-              <thead>
-                <tr>
-                  <th scope="col">ID</th>
-                  <th scope="col">Movie Name</th>
-                  <th scope="col">Movie Price</th>
-                </tr>
-              </thead>
-              <tbody>
-                {props.moviesData.map((ele, index) => (
+            <div className="card pt-4 pb-4 px-5 shadow">
+              <h3 className="text-center mb-5">MOVIES DATA</h3>
+              <label htmlFor="search">
+                <input
+                  id="search"
+                  type="text"
+                  className="ps-2"
+                  placeholder="Seach"
+                />
+              </label>
+              <table class="table  mt-4 table-bordered">
+                <thead>
                   <tr>
-                    <th scope="row">{index + 1}</th>
-                    <td>{ele.name}</td>
-                    <td>{ele.price}</td>
+                    <th scope="col">ID</th>
+                    <th scope="col">Movie Name</th>
+                    <th scope="col">Movie Price</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {props.moviesData.map((ele, index) => (
+                    <tr>
+                      <th scope="row">{index + 1}</th>
+                      <td>{ele.name}</td>
+                      <td>{ele.price}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
